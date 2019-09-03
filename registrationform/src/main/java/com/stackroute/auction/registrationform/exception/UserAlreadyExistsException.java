f31e8a815f1d1d3269b992c5362b025720de2dd8
VishnuPriya2297 <davu.vishnupriya2297@gmail.com>
2019-09-03 10:04:39 +0530

    registration microservice backend

--- registrationform/src/main/java/com/stackroute/auction/registrationform/exception/UserAlreadyExistsException.java
+++
@@ -1,6 +0,0 @@
-package com.stackroute.auction.registrationform.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-}
