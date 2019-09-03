87791b6719fbc7fc6ac353a1c22803121032a3f8
Nikhila Talla <45334443+tls97nikhila@users.noreply.github.com>
2019-09-03 17:59:29 +0530

    Update Category.java

--- recommendationMicroService/src/main/java/com/stackroute/springbootneo4jexample1/model/Category.java
+++ recommendationMicroService/src/main/java/com/stackroute/springbootneo4jexample1/model/Category.java
@@ -6,6 +6,7 @@ import org.neo4j.ogm.annotation.Relationship;
 import java.util.List;
 
 @NodeEntity
+// Category entity with BELONGS_TO relationship with products
 public class Category {
     private String name;
     @Relationship(type = "BELONGS_TO", direction = Relationship.INCOMING)
