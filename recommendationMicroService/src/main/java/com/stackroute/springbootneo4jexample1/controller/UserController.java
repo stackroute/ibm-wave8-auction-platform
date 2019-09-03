e037993e6baf3b16835859055658fc0a9b2defc6
Nikhila Talla <45334443+tls97nikhila@users.noreply.github.com>
2019-09-03 17:03:53 +0530

    Comments added

--- recommendationMicroService/src/main/java/com/stackroute/springbootneo4jexample1/controller/UserController.java
+++ recommendationMicroService/src/main/java/com/stackroute/springbootneo4jexample1/controller/UserController.java
@@ -13,33 +13,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1")  //REST end-point
 @CrossOrigin("*")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-
+    //Returns all the users
     @GetMapping("/users")
     public Collection<User> getAll() {
         return userService.getAll();
     }
 
-
+//Returns all the Liked categories of a user
     @GetMapping("/userslikes")
     public Collection<Category> getAllLikes() {
         return userService.getAllLikes();
     }
-
+//Returns all the recommended products of a user
     @GetMapping("/userrcm")
     public Collection<Product> getAllRec() {
         return userService.getAllRecm();
     }
-
+//Get all the liked products of a user
     @GetMapping("/myproducts")
     public Collection<WishlistProduct> getMyFavProducts(){ return userService.getMyFavs(); }
-
+//Post a product to wishlist
     @PostMapping("/savewishlist")
     public ResponseEntity<User> saveWishlist(@RequestBody WishlistProduct wish){
 
@@ -48,6 +48,8 @@ public class UserController {
         responseEntity=new ResponseEntity("Succesfully Created", HttpStatus.CREATED);
         return responseEntity;
     }
+    
+    //Remove from wishlist
     @PostMapping("/removefav")
     public ResponseEntity<?> deleteTrackByname(@RequestBody WishlistProduct wish)  {
        Collection<WishlistProduct> wishlist;
