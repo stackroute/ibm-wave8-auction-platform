b65b443e42595386d22be9d3624628ba74d7e148
VishnuPriya2297 <51954142+VishnuPriya2297@users.noreply.github.com>
2019-09-04 09:19:35 +0530

    Removed commented codes

--- registrationMicroservice/src/main/java/com/stackroute/auction/registrationform/config/kafkaConfiguration.java
+++ registrationMicroservice/src/main/java/com/stackroute/auction/registrationform/config/kafkaConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-//@Configuration
 public class kafkaConfiguration {
     @Bean
     public ProducerFactory<String, User> producerFactory() {
